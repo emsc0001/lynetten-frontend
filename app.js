ff795076266b1fab6ed36d070faaf55ffa9a48bb
Abdiox <abdirahmanroble2610@gmail.com>
2023-11-28 19:33:02 +0100

    yes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+"use strict"
+
+
+
 document.addEventListener("click", function (e) {
   if (e.target.classList.contains("dropbtn")) {
     var dropdown = e.target.nextElementSibling;
@@ -12,6 +16,17 @@ document.addEventListener("click", function (e) {
   }
 });
 
+function toggleSearchBar() {
+  const searchBar = document.querySelector(".search-bar");
+  searchBar.style.display = searchBar.style.display === "block" ? "none" : "block";
+}
+
+function search() {
+  // Implement your search logic here
+  const searchInput = document.getElementById("searchInput").value;
+  alert(`Search query: ${searchInput}`);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   let navItems = document.querySelectorAll(".nav-item");
 
@@ -25,45 +40,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-// Tab til at skifte fra forside til handelsbetingelser
-// document.addEventListener("DOMContentLoaded", function () {
-//   let navItems = document.querySelectorAll(".nav-item");
-
-//   navItems.forEach(function (item) {
-//     item.addEventListener("click", function (event) {
-//       event.preventDefault();
-
-//       // Hent det data-tab-attribut fra det klikkede element
-//       let tab = item.getAttribute("data-tab");
-
-//       // Skjul alt indhold
-//       hideAllContent();
-
-//       // Vis det indhold, der svarer til den klikkede fane
-//       showContent(tab);
-
-//       // Fjern 'active' klassen fra alle faner og tilføj den til den aktive fane
-//       navItems.forEach(function (navItem) {
-//         navItem.classList.remove("active");
-//       });
-//       item.classList.add("active");
-//     });
-//   });
-
-//   // Skjul alt indhold
-//   function hideAllContent() {
-//     let contents = document.querySelectorAll(".tab-content");
-//     contents.forEach(function (content) {
-//       content.style.display = "none";
-//     });
-//   }
-
-//   // Vis specifikt indhold
-//   function showContent(target) {
-//     let content = document.getElementById(target);
-//     if (content) {
-//       content.style.display = "block";
-//     }
-//   }
-// });
