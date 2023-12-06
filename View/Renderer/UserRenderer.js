231e3f305475d505aab4800d9a7cefc7b7e43faa
Abdiox <abdirahmanroble2610@gmail.com>
2023-12-06 16:43:26 +0100

    fået linket backend bruger til frontend

--- View/Renderer/UserRenderer.js
+++ View/Renderer/UserRenderer.js
@@ -5,24 +5,22 @@ export default class UserRenderer extends ItemRenderer {
   render() {
     const user = this.item;
     const html = /*html*/ `
-        <div class="user">
-            <h3>${user.email}</h3>
-            <h3>${user.password}</h3>
-            <h3>${user.newsletterSubscription}</h3>
-        </div>
-        `;
-
-    // <h2>
-    //   ${user.firstName} ${user.lastName}
-    // </h2>;
+      <table class="user-info">
+        <thead>
+          <tr>
+            <th>${user.userId}</th>
+            <th>${user.email}</th>
+            <th>${user.password}</th>
+            <th>${user.newsletterSubscription}</th>
+          </tr>
+        </thead>
+      </table>
+    `;
 
     return html;
   }
 
   postRender(element) {
-    // element.querySelector(".nav-item a").addEventListener("click", (event) => {
-    //   event.preventDefault();
-    //   controller.getProductsByCategory(element.categoryId);
-    // });
+    // Tilføj eventuelle efterbehandling her, hvis nødvendigt
   }
 }
