eb5c6971257ba2a349e06b42698ab52485bc51f9
MadsFolkmann <mads1238@gmail.com>
2023-12-06 01:23:28 +0100

    Can add and remove items, and kurv.html done

---
+++ View/Renderer/ProductCartRenderer.js
@@ -0,0 +1,115 @@
+import ItemRenderer from "./Itemrenderer.js"
+import { cart, initializeCartView, saveCartToLocalStorage, htmlSide, initializeCartHtmlView } from "../../main.js";
+
+export default class ProductCartRenderer extends ItemRenderer {
+    render() {
+        const cartItem = this.item;
+        const total = cartItem.listPrice * cartItem.quantity; // Calculate the total price for the cart item
+        const html = `
+      <div class="cart-item">
+        <img src="${cartItem.imageURLs}" alt="${cartItem.productName}">
+        <h2>${cartItem.productName}</h2>
+        <h3>${cartItem.listPrice}kr</h3>
+        <div class="quantity">
+          <button class="minus-button" data-id="${cartItem.productId}">-</button>
+          <input type="number" value="${cartItem.quantity}" disabled>
+          <button class="plus-button" data-id="${cartItem.productId}">+</button>
+        </div>
+        <p>Total: ${total}kr</p>
+        <button class="remove-button" data-id="${cartItem.productId}">Fjern</button>
+      </div>
+    `;
+
+        return html;
+    }
+
+    renderTotalPriceCart() {
+        const cartTotal = ProductCartRenderer.calculateTotal(cart); // Calculate the total price for the cart
+        const totalPriceHTML = `
+            <div class="cart-total">
+                <p>Total Pris: ${cartTotal} kr</p>
+                <a href="kurv.html" class="go-to-cart-link">Gå til kurv</a>
+            </div>
+        `;
+        return totalPriceHTML;
+    }
+
+    renderTotalPriceCartHtml() {
+        const cartTotal = ProductCartRenderer.calculateTotal(cart); // Calculate the total price for the cart
+        const totalPriceHTML = `
+        <p>Total: ${cartTotal}kr</p>
+        <button class="checkout-button">Gå til betaling</button>
+        `;
+        return totalPriceHTML;
+    }
+
+
+    static calculateTotal(cart) {
+        let total = 0;
+        cart.forEach((item) => {
+            total += item.listPrice * item.quantity;
+        });
+        return total;
+    }
+
+    postRender(element) {
+        const productId = this.item.productId; // Assuming productId is part of the cart item
+
+        // Decrease quantity button event
+        element.querySelector(".minus-button").addEventListener("click", () => {
+            const cartItem = cart.find((item) => item.productId === productId);
+            if (cartItem && cartItem.quantity > 1) {
+                cartItem.quantity--;
+                this.render(); // Rerender the cart after updating the quantity
+                saveCartToLocalStorage(); // Save the updated cart to localStorage
+                if (htmlSide === '/kurv.html') {
+                    initializeCartHtmlView();
+                } else {
+                    initializeCartView();
+                }
+            } else if (cartItem && cartItem.quantity === 1) { // If the quantity is 1, remove the item from the cart
+                const cartIndex = cart.findIndex((item) => item.productId === productId);
+                if (cartIndex !== -1) {
+                    cart.splice(cartIndex, 1); // Remove the item from the cart
+                    this.render(); // Rerender the cart after removing the item
+                    saveCartToLocalStorage(); // Save the updated cart to localStorage
+                    if (htmlSide === '/kurv.html') {
+                        initializeCartHtmlView();
+                    } else {
+                        initializeCartView();
+                    }
+                }
+            }
+        });
+
+        // Increase quantity button event
+        element.querySelector(".plus-button").addEventListener("click", () => {
+            const cartItem = cart.find((item) => item.productId === productId);
+            if (cartItem) {
+                cartItem.quantity++;
+                this.render(); // Rerender the cart after updating the quantity
+                saveCartToLocalStorage(); // Save the updated cart to localStorage
+                if (htmlSide === "/kurv.html") {
+                    initializeCartHtmlView();
+                } else {
+                    initializeCartView();
+                }
+            }
+        });
+
+        // Remove item button event
+        element.querySelector(".remove-button").addEventListener("click", () => {
+            const cartIndex = cart.findIndex((item) => item.productId === productId);
+            if (cartIndex !== -1) {
+                cart.splice(cartIndex, 1); // Remove the item from the cart
+                this.render(); // Rerender the cart after removing the item
+                saveCartToLocalStorage(); // Save the updated cart to localStorage
+                if (htmlSide === "/kurv.html") {
+                    initializeCartHtmlView();
+                } else {
+                    initializeCartView();
+                }
+            }
+        });
+    }
+}
--- View/Renderer/ProductRenderer.js
+++ View/Renderer/ProductRenderer.js
@@ -20,28 +20,29 @@ export default class ProductRenderer extends ItemRenderer {
     }
 
     postRender(element) {
+            const product = this.item;
         element.querySelector(".button").addEventListener("click", async (event) => {
             event.preventDefault();
-
-            // Checks if the guest order is already created
-            if (!controller.guestOrderCreated.value) {
+            try {
                 const orderDate = new Date().toISOString().slice(0, 10);
-                try {
-                    const guestOrderId = await guestOrderController.createGuestOrder(orderDate);
-                    console.log(guestOrderId);
-                    if (guestOrderId) {
-                        // Marks the guest order as created and adds the item to the cart
-                        controller.guestOrderCreated.value = true;
-                        controller.addToCart(this.item.productId, this.item.listPrice);
-                    } else {
-                        console.error("Failed to create guest order");
-                    }
-                } catch (error) {
-                    console.error("Error creating guest order:", error);
+                const guestOrderId = await guestOrderController.createGuestOrder(orderDate);
+                // Checks if the guest order is already created
+                if (!controller.guestOrderCreated.value) {
+                        console.log(guestOrderId);
+                        if (guestOrderId) {
+                            // Marks the guest order as created and adds the item to the cart
+                            controller.guestOrderCreated.value = true;
+                            controller.addToCart(product.productId, product.listPrice, product.productName, product.imageURLs, guestOrderId);
+                        } else {
+                            console.error("Failed to create guest order");
+                        }
+
+                } else {
+                    // If the guest order is already created, adds the item to the cart directly
+                    controller.addToCart(product.productId, product.listPrice, product.productName, product.imageURLs, guestOrderId);
                 }
-            } else {
-                // If the guest order is already created, adds the item to the cart directly
-                controller.addToCart(this.item.productId, this.item.listPrice);
+            } catch (error) {
+                console.error("Error creating guest order:", error);
             }
         });
     }
