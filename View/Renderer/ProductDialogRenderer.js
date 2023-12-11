111360fb44433cbe91a01233de22de7b6d7414d0
Abdiox <abdirahmanroble2610@gmail.com>
2023-12-12 00:49:19 +0100

    deleted

--- View/Renderer/ProductDialogRenderer.js
+++ View/Renderer/ProductDialogRenderer.js
@@ -1,25 +1,25 @@
-import ItemRenderer from "./Itemrenderer.js";
-import * as controller from "../../main.js";
+// import ItemRenderer from "./Itemrenderer.js";
+// import * as controller from "../../main.js";
 
-export default class ProductDialogRenderer extends ItemRenderer {
-  render() {
-    const productDialog = this.item;
-    const html = /*html*/ `
-<table class="product-info">
-<thead>
-<tr>
-<th> ${productDialog.productId} </th>
-<th src="${productDialog.imageURLs}"</th> 
-<th>${productDialog.productNumber}</th>
-<th>${productDialog.productName}</th>
-<th>${productDialog.listPrice}</th>
-<th>${productDialog.offerPrice}</th>
-<th>${productDialog.stockQuantity}</th>
-<th>${productDialog.description}</th>
-</tr>
-`;
-    return html;
-  }
+// export default class ProductDialogRenderer extends ItemRenderer {
+//   render() {
+//     const productDialog = this.item;
+//     const html = /*html*/ `
+// <table class="product-info">
+// <thead>
+// <tr>
+// <th> ${productDialog.productId} </th>
+// <th src="${productDialog.imageURLs}"</th>
+// <th>${productDialog.productNumber}</th>
+// <th>${productDialog.productName}</th>
+// <th>${productDialog.listPrice}</th>
+// <th>${productDialog.offerPrice}</th>
+// <th>${productDialog.stockQuantity}</th>
+// <th>${productDialog.description}</th>
+// </tr>
+// `;
+//     return html;
+//   }
 
-  postRender(element) {}
-}
+//   postRender(element) {}
+// }
