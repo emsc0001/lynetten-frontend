ac733452ef51c306cf186125191aa703145d5dcd
Emil Schlichter <124031667+emsc0001@users.noreply.github.com>
2023-12-11 14:42:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -304,6 +304,14 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+// MAP
+let myMap = L.map("interactive-map").setView([55.691046, 12.599752], 13); // Replace with your coordinates
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap contributors",
+}).addTo(myMap);
+
 
 
 
