33e51a3945b152a905faf7cf8df8bb9fc5b03a49
Abdiox <abdirahmanroble2610@gmail.com>
2023-12-12 00:43:42 +0100

    yes

--- View/Dialogs/ProductsDialog.js
+++ View/Dialogs/ProductsDialog.js
@@ -1,35 +1,16 @@
 import Dialog from "./Dialog.js";
-import Product from "../../Model/Product.js";
-import * as Controller from "../../Model/Rest-services/user-rest.js";
 
-export default class ProductsDialog extends Dialog {
-  renderHTML() {
+export default class ProductDialog extends Dialog {
+  renderHTML(product) {
     const html = /*html*/ `
-        <div class="product-form-container">
-          <form id="productForm">
-            <button type="button" data-action="close">X</button>
-            <h1 class="productId">Produkt ID</h1>
-            <h2 class="productHeader">Produkt Information</h2>
-            <label for="productNumber">Produktnummer</label>
-            <input type="text" name="productNumber" id="productNumber" required>
-            <label for="productName">Produktnavn</label>
-            <input type="text" name="productName" id="productName" required>
-            <label for="imageURLs">Billed-URLs</label>
-            <textarea name="imageURLs" id="imageURLs" required></textarea>
-            <label for="listPrice">Listepris</label>
-            <input type="number" name="listPrice" id="listPrice" step="0.01" required>
-            <label for="offerPrice">Tilbudspris</label>
-            <input type="number" name="offerPrice" id="offerPrice" step="0.01">
-            <label for="stockQuantity">Lagermængde</label>
-            <input type="number" name="stockQuantity" id="stockQuantity" required>
-            <label for="description">Beskrivelse</label>
-            <textarea name="description" id="description" required></textarea>
-            <button id="lukProdukt"  data-action="cancel">Luk Produkt</button>
-          </form>
-        </div>
-        `;
+      <div class="product-details">
+        <h4>${product.productName}</h4>
+        <p>Description: ${product.description}</p>
+        <p>Price: ${product.listPrice}</p>
+        <p>Category: ${product.categoryName}</p>
+      </div>
+      <button data-action="close">Close</button>
+    `;
     return html;
   }
-
-  postRender() {}
 }
