d8d2934aa8eee7fe59799c697832273765e12521
Abdiox <abdirahmanroble2610@gmail.com>
2023-12-12 02:15:51 +0100

    feedback på lager status af varer

--- View/Dialogs/ProductDialog.js
+++ View/Dialogs/ProductDialog.js
@@ -1,20 +1,27 @@
 import Dialog from "./Dialog.js";
-
 export default class ProductDialog extends Dialog {
   renderHTML(product) {
+    const isLowInStock = product.stockQuantity > 0 && product.stockQuantity <= 5;
+    const stockFeedback = isLowInStock
+      ? `<p style="color: red; font-weight: bold;">Lavt på lager🚨</p>`
+      : product.stockQuantity === 0
+      ? `<p style="color:  #FFD700; font-weight: bold;">Ingen varer på lager⚠️</p>`
+      : `<p>På lager</p>`;
+
     const html = /*html*/ `
-  <div class="product-details">
-    <img src="${product.imageURLs}" alt="${product.productName}">
-    <h4>${product.productName}</h4>
-    <div id="description-container">
-      <p>Beskrivelse: ${product.description}</p>
-    </div>
-    <p>Pris: ${product.listPrice} Kr</p>
-    <p>Kategori: ${product.categoryName}</p>
-    <p>Varer på lager ${product.stockQuantity}</p>
-    <button type="button" class="closeModal" data-action="close">Luk</button>
-  </div>
-`;
+      <div class="product-details">
+        <img src="${product.imageURLs}" alt="${product.productName}">
+        <h4>${product.productName}</h4>
+        <p>${stockFeedback}</p>
+        <div id="description-container">
+          <p>Beskrivelse: ${product.description}</p>
+        </div>
+        <p>Pris: ${product.listPrice} Kr</p>
+        <p>Kategori: ${product.categoryName}</p>
+        <p>Varer på lager ${product.stockQuantity}</p>
+        <button type="button" class="closeModal" data-action="close">Luk</button>
+      </div>
+    `;
 
     return html;
   }
