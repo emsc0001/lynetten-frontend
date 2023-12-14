64643e743127a0cc9fc91e4bf7ae6e641c894b0b
Abdiox <abdirahmanroble2610@gmail.com>
2023-12-14 13:08:59 +0100

    SORTT VIRKER

--- View/Renderer/Paginater.js
+++ View/Renderer/Paginater.js
@@ -65,4 +65,33 @@ export default class Paginater extends ListRenderer {
       });
     });
   }
+  sort(sortBy, sortDir) {
+    if (sortBy === this.sortBy) {
+      this.sortDir = this.sortDir === "asc" ? "desc" : "asc";
+    } else {
+      if (sortDir) {
+        this.sortDir = sortDir;
+      } else {
+        this.sortDir = "asc";
+      }
+    }
+    // store sortBy in property for next time
+    this.sortBy = sortBy;
+
+    // make direction into a number, to make it easier to flip
+    const dir = this.sortDir === "asc" ? 1 : -1;
+
+    // NOTE: sortFunctions MUST be arrow-functions, to keep the reference to this!
+    const valueSortFunction = (a, b) => (a.item[this.sortBy] > b.item[this.sortBy] ? dir : -dir);
+    const stringSortFunction = (a, b) => a.item[this.sortBy]?.localeCompare(b.item[this.sortBy]) * dir;
+
+    // select between sortFunctions, depending on the type on the sortBy property in the first item in the list
+    const sortFunction = typeof this.list[0].item[this.sortBy] === "string" ? stringSortFunction : valueSortFunction;
+
+    // sort the list with the chosen sortFunction
+    this.list.sort(sortFunction);
+
+    // and re-render the list
+    this.render();
+  }
 }
