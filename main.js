5b13e03ca1ae59da1108494ae2e570801304dd7a
MadsFolkmann <mads1238@gmail.com>
2023-12-15 01:52:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -91,8 +91,8 @@ async function baddServiceApp() {
 
 //Initiliaze views for koebeguide.html, handelsBetingelser and index.html
 async function initializeOtherHtmlViews() {
+  console.log("You are on the otherHtml page");
     // initialize Category Views //
-   const hey = document.querySelector(".category-list")
     categoriesLists = new ListRenderer(categories, ".category-list", CategoryRenderer);
   categoriesLists.render();
 
@@ -140,6 +140,7 @@ async function initializeOtherHtmlViews() {
 
 //------------Initiliaze views for products.html---------------//
 function initializeProductViews() {
+  console.log("You are on the products page");
   productsLists = new Paginater(products, "#products-container", ProductRenderer, 10);
   productsLists.render();
 
